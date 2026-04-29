dab80a490760e89bea2ed629e9a1f8698b5935b4
yvora-gastronomia <yvorafoods@gmail.com>
2026-04-29 03:16:46 -0300

    Add dedicated band login page

---
+++ pages/1_Banda.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="YVORA Music | Banda", layout="wide")
+st.markdown("""
+<meta http-equiv="refresh" content="0; url=/?view=banda&sid=bossa-nova-yvora">
+<script>window.location.href='/?view=banda&sid=bossa-nova-yvora';</script>
+""", unsafe_allow_html=True)
+st.info("Redirecionando para a visão da banda...")
